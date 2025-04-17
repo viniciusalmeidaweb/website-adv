e7bff888f21f7e498a8185a7aff8593568493431
viniweb <viniminimal@gmail.com>
2025-04-17 20:31:08 -0300

    html equipe

--- website/views.py
+++ website/views.py
@@ -10,12 +10,16 @@ class HomeListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        especialidade = Especialidades.objects.all()  # Aqui, você pode adicionar qualquer outro queryset
-        context['especialidade_list'] = especialidade  # O nome que o objeto terá no template
+        
+        # Adiciona as especialidades
+        context['especialidade_list'] = Especialidades.objects.all()  # O nome que o objeto terá no template
+        
+        # Adiciona a equipe
+        context['equipe_list'] = Equipe.objects.all()  # O nome que o objeto terá no template
+
         return context
-
-
-
+    
+      
 class EspecialidadeDetailView(DetailView):
     model = Especialidades
     template_name = 'especialidade_detail.html' # Template onde será renderizada a view
@@ -26,5 +30,4 @@ class EspecialidadeDetailView(DetailView):
         especialidades_lista = Especialidades.objects.all()  # Aqui, você pode adicionar qualquer outro queryset
         context['especialidade_lista_itens'] = especialidades_lista  # O nome que o objeto terá no template
         return context
-
 
