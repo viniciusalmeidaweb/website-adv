3148cf9067e514a1dd8b9e560deea720ccce0d36
viniweb <viniminimal@gmail.com>
2025-06-24 14:26:51 -0300

    settings update

--- app/settings.py
+++ app/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
