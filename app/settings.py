cb1dda3c0f22fe541137fdbedb834624f6acb4db
viniweb <viniminimal@gmail.com>
2025-06-24 22:46:17 -0300

    update settings

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!=61jr#!w+22)@f1l9&9-hnf-a!^&l)cp9)v@uxx%zahj4b^xa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
