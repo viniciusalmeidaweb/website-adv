519a39157fb95537e1231e69196c79e4c4487db0
viniweb <viniminimal@gmail.com>
2025-07-02 14:51:50 -0300

    ajustes views e templates

--- website/views.py
+++ website/views.py
@@ -15,12 +15,13 @@ class ContextoPadraoMixin:
         context['categoria_list'] = Categoria.objects.all()
         return context
   
-
+# Página inicial
 class HomeListView(ContextoPadraoMixin, ListView):
     model = Sobre
     template_name = 'index.html'
 
 
+# Lista de posts do blog (somente publicados)
 class BlogListView(ContextoPadraoMixin, ListView):
     model = Post
     template_name = 'blog.html'
@@ -29,45 +30,50 @@ class BlogListView(ContextoPadraoMixin, ListView):
     def get_queryset(self):
         return Post.objects.filter(publicado=True).order_by('-data_criacao')
     
-  
+
+# Detalhes de um post do blog, com slug da categoria + post
 class BlogDetailView(ContextoPadraoMixin, DetailView):
     model = Post
     template_name = 'blog_detail.html'
     context_object_name = 'post_detail'
 
     def get_object(self):
-        categoria_slug = self.kwargs['categoria_slug']
-        post_slug = self.kwargs['post_slug']
-        return get_object_or_404(Post, slug=post_slug, categoria__slug=categoria_slug)
+        return get_object_or_404(
+            Post,
+            slug=self.kwargs['post_slug'],
+            categoria__slug=self.kwargs['categoria_slug'],
+            publicado=True  # Garante que só exibe posts publicados
+    )
 
-
+# Lista de posts filtrados por categoria
 class CategoriaDetailView(ContextoPadraoMixin, ListView):
     model = Post
     template_name = 'categoria_detail.html'
     context_object_name = 'category_post'
 
     def get_queryset(self):
-        categoria_slug = self.kwargs['slug']
-        self.categoria = get_object_or_404(Categoria, slug=categoria_slug)
-        return Post.objects.filter(categoria__slug=categoria_slug)  
+        self.categoria = get_object_or_404(Categoria, slug=self.kwargs['slug'])
+        return Post.objects.filter(categoria=self.categoria, publicado=True).order_by('-data_criacao')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # ← chama ContextoPadraoMixin + ListView
-        context['categoria'] = self.categoria          # ← adiciona só o que é específico da view
+        context = super().get_context_data(**kwargs)
+        context['categoria'] = self.categoria
         return context
 
-    
+# Página "Quem somos"   
 class SobreListView(ContextoPadraoMixin, ListView):
     model = Sobre
     template_name = 'quem_somos.html'
     context_object_name = 'sobre_list'
 
 
+# Lista de especialidades
 class EspecialidadesListView(ContextoPadraoMixin, ListView):
     model = Especialidades
     template_name = 'especialidades.html'
 
 
+# Detalhe de uma especialidade
 class EspecialidadeDetailView(ContextoPadraoMixin, DetailView):
     model = Especialidades
     template_name = 'especialidade_detail.html'
@@ -82,11 +88,9 @@ class ContatoView(FormView):
     def form_valid(self, form):
         # Aqui você pode enviar email, salvar no banco, etc.
         nome = form.cleaned_data['nome']
-        email = form.cleaned_data['email']
-        telefone = form.cleaned_data['telefone']
         mensagem = form.cleaned_data['mensagem']
 
         # Exemplo: imprimir no console
-        print(f"Mensagem de {nome}, {telefone}, <{email}>: {mensagem}")
+        print(f"Mensagem de {nome}, {mensagem}")
 
         return super().form_valid(form)
